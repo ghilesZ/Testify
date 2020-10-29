ac6002756fa19105bb2b90d321ea66a806429d59
ghilesZ <ghiles.ziat@gmail.com>
2020-10-29 11:26:26 +0100

    exemple of compiling a box into a generator

--- src/abstract.ml
+++ src/abstract.ml
@@ -3,19 +3,23 @@
 
 open Migrate_parsetree
 open Ast_410
-open Parsetree
-open Ast_mapper
 open Ast_helper
 open Helper
 open Apron
 
+module Conv = Convert (OCaml_410) (OCaml_current)
+
+let manager = Box.manager_alloc()
+
 let s_to_mpqf = let open Scalar in function
-  | Mpqf x -> x
-  | Float x -> Mpqf.of_float x
-  | Mpfrf x -> Mpfrf.to_mpqf x
+                                | Mpqf x -> x
+                                | Float x -> Mpqf.of_float x
+                                | Mpfrf x -> Mpfrf.to_mpqf x
 
 let itv_to_mpqf i = Interval.(s_to_mpqf i.inf,s_to_mpqf i.sup)
 
+(* compile an itv into a parsetree expression corresponding to a
+   generator *)
 let compile_itv typ (i:Interval.t) =
   let inf,sup = itv_to_mpqf i in
   match typ with
@@ -24,18 +28,48 @@ let compile_itv typ (i:Interval.t) =
      let size = size+1 in
      let inf = inf |> Mpqf.to_float |> ceil |> int_of_float in
      let body =
-       let r = apply_nolbl_s "Gen.int_bound" [int_exp size] in
+       let gen = apply_nolbl_s "Gen.int_bound" [int_exp size] in
+       let r = apply_nolbl gen [exp_id "rand_state"] in
        apply_nolbl_s "+" [int_exp inf; r]
      in
-     Exp.fun_ Nolabel None (pat_s "x") body
+     Exp.fun_ Nolabel None (pat_s "rand_state") body
   | Environment.REAL ->
      let size = Mpqf.sub sup inf |> Mpqf.to_float in
      let inf = inf |> Mpqf.to_float in
      let body =
-       let r = apply_nolbl_s "Gen.float_bound_exclusive" [float_exp 1.] in
-       let r = apply_nolbl_s "*." [r; float_exp size] in
-       apply_nolbl_s "+." [float_exp inf; r]
+       let gen = apply_nolbl_s "Gen.float_bound_exclusive" [float_exp 1.] in
+       let value = apply_nolbl gen [exp_id "rand_state"] in
+       let r = apply_nolbl_s "Float.mul" [value; float_exp size] in
+       apply_nolbl_s "Float.add" [float_exp inf; r]
      in
-     Exp.fun_ Nolabel None (pat_s "x") body
+     Exp.fun_ Nolabel None (pat_s "rand_state") body
 
-let compile_box _b = assert false
+(** builds a Parsetree.expression corresponding to a generator from a
+   box *)
+let compile_box b =
+  let box = Abstract1.to_box manager b in
+  let env = box.Abstract1.box1_env in
+  let itvs = box.Abstract1.interval_array in
+  let instance,_ =
+    Array.fold_left (fun (acc,idx) i ->
+        let v = Environment.var_of_dim env idx in
+        let typ = Environment.typ_of_var env v in
+        let value = apply_nolbl (compile_itv typ i) [exp_id "rand_state"] in
+        let pair = Exp.tuple [string_exp (Var.to_string v);value] in
+        let instance = cons_exp pair acc in
+        instance,(idx+1)
+      ) (empty_list_exp,0) itvs
+  in
+  Exp.fun_ Nolabel None (pat_s "rand_state") instance
+
+let _ =
+  let vx = Var.of_string "x" in
+  let vy = Var.of_string "y" in
+  let env = Environment.make [||] [|vx;vy|] in
+  let box = Abstract1.top manager env in
+  let texpr1 = Texpr1.cst env (Coeff.i_of_int (-3) 5) in
+  let texpr2 = Texpr1.cst env (Coeff.i_of_int 2 18) in
+  let box = Abstract1.assign_texpr manager box vx texpr1 None in
+  let box = Abstract1.assign_texpr manager box vy texpr2 None in
+  let ast = compile_box box in
+  Format.printf "%a\n%!" Pprintast.expression (Conv.copy_expression ast)
