9a2ceeaaf6750f0db4ee760f655bbb7c3b539c06
Martin Pépin <kerl@wkerl.me>
2022-01-26 16:59:39 +0100

    More elaborate mutual test

--- examples/recursive/mutual.ml
+++ examples/recursive/mutual.ml
@@ -1,3 +1,8 @@
-type desc = Int of int | Add of t * t
+type pos = (int[@satisfying fun x -> 0 <= x])
+
+type desc = Int of pos | Add of t * t
 
 and t = {annotation: int; desc: desc}
+
+let rec eval (x : t) : pos =
+  match x.desc with Int n -> n | Add (x, y) -> eval x + eval y
